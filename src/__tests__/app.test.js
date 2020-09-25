011f1bbbada8a1113618abdf2e6a2aad06c2664e
jd <jackethdavies@hotmail.com>
2020-09-25 22:14:44 +0100

    removing un-used import from app.test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { setupServer } from 'msw/node'
 import { rest } from 'msw'
